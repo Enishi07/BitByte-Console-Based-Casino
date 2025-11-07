58ba5c5b77c98bdf7aef604980222f2341d6f66a
Tachymama <rodwardluciofernandolabog@gmail.com>
2025-11-08 00:12:24 +0800

    UI Changes
    changes Ui in the main

--- BitByte_Casino/BitByteCasino.java
+++ BitByte_Casino/BitByteCasino.java
@@ -13,18 +13,26 @@ public class BitByteCasino {
         ColorGame colorGame = new ColorGame();
 
         System.out.println(" Welcome to the Mini Java Casino! ");
+
+        System.out.println("__________.__  __ __________          __           _________               .__               \r\n" + //
+                        "\\______   \\__|/  |\\______   \\___.__._/  |_  ____   \\_   ___ \\_____    _____|__| ____   ____  \r\n" + //
+                        " |    |  _/  \\   __\\    |  _<   |  |\\   __\\/ __ \\  /    \\  \\/\\__  \\  /  ___/  |/    \\ /  _ \\ \r\n" + //
+                        " |    |   \\  ||  | |    |   \\\\___  | |  | \\  ___/  \\     \\____/ __ \\_\\___ \\|  |   |  (  <_> )\r\n" + //
+                        " |______  /__||__| |______  // ____| |__|  \\___  >  \\______  (____  /____  >__|___|  /\\____/ \r\n" + //
+                        "        \\/                \\/ \\/                \\/          \\/     \\/     \\/        \\/        ");
+        
         do {
-            System.out.println("===========================================");
-            System.out.printf("Balance: PHP%.2f \n" ,balance);
-            System.out.println("===========================================");
-            System.out.println("Choose a game:");
-            System.out.println("1.  Dice Roll");
-            System.out.println("2.  Blackjack");
-            System.out.println("3.  Slot Machine");
-            System.out.println("4.  Color Game");
-            System.out.println("5.  Exit");
-            System.out.println("-----------------------------------------------");
-            System.out.print("Enter your choice: ");
+            System.out.println("==============================================================================================");
+            System.out.printf("\t\t\t\t\tBalance: PHP%.2f \n" ,balance);
+            System.out.println("==============================================================================================");
+            System.out.println("\t\t\t\t\tChoose a game:");
+            System.out.println("\t\t\t\t\t1.  Dice Roll");
+            System.out.println("\t\t\t\t\t2.  Blackjack");
+            System.out.println("\t\t\t\t\t3.  Slot Machine");
+            System.out.println("\t\t\t\t\t4.  Color Game");
+            System.out.println("\t\t\t\t\t5.  Exit");
+            System.out.println("----------------------------------------------------------------------------------------------");
+            System.out.print("\t\t\tEnter your choice: ");
             choice = sc.nextInt();
 
             switch (choice) {
