2bae4a7ba7e6949cc8118399df9758832698786d
enishi <jarenkengabriel@gmail.com>
2025-11-07 22:31:45 +0800

    Deletion of comment
    Deletion of coment

--- BitByte_Casino/BlackjackGame.java
+++ BitByte_Casino/BlackjackGame.java
@@ -1,4 +1,3 @@
-// 🃏 Blackjack Game
 // 🃏 Enhanced Blackjack Game (Real Rules)
 class BlackjackGame extends BitByteCasinoGame {
     private String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
