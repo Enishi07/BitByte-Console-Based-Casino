0d500a4f92eb2e6c06754533378fa3966dec6924
enishi <jarenkengabriel@gmail.com>
2025-11-13 20:14:37 +0800

    Correction of Menu Logo
    Due to repository Commit and push error, the new menu logo was replaced and the original try do syntax was deleted

--- BitByte_Casino/BitByteCasino.java
+++ BitByte_Casino/BitByteCasino.java
@@ -15,19 +15,22 @@ public class BitByteCasino {
         CrazyTimeGame crazyTime = new CrazyTimeGame();
         BlackRedRouletteGame rouletteGame = new BlackRedRouletteGame();
 
-         System.out.println(" /$$$$$$$  /$$   /$$     /$$$$$$$              /$$                      /$$$$$$                      /$$\r\n" + 
-                        "| $$__  $$|__/  | $$    | $$__  $$            | $$                     /$$__  $$                    |__/\r\n" + 
-                        "| $$  \\ $$ /$$ /$$$$$$  | $$  \\ $$ /$$   /$$ /$$$$$$    /$$$$$$       | $$  \\__/  /$$$$$$   /$$$$$$$ /$$ /$$$$$$$   /$$$$$$\\ \r\n" + 
-                        "| $$$$$$$ | $$|_  $$_/  | $$$$$$$ | $$  | $$|_  $$_/   /$$__  $$      | $$       |____  $$ /$$_____/| $$| $$__  $$ /$$__  $$\r\n" + 
-                        "| $$__  $$| $$  | $$    | $$__  $$| $$  | $$  | $$    | $$$$$$$$      | $$        /$$$$$$$|  $$$$$$ | $$| $$  \\ $$| $$  \\ $$\r\n" + 
-                        "| $$  \\ $$| $$  | $$ /$$| $$  \\ $$| $$  | $$  | $$ /$$| $$_____/      | $$    $$ /$$__  $$ \\____  $$| $$| $$  | $$| $$  | $$\r\n" +
-                        "| $$$$$$$/| $$  |  $$$$/| $$$$$$$/|  $$$$$$$  |  $$$$/|  $$$$$$$      |  $$$$$$/|  $$$$$$$ /$$$$$$$/| $$| $$  | $$|  $$$$$$/\r\n" +
-                        "|_______/ |__/   \\___/  |_______/  \\____  $$   \\___/   \\_______/       \\______/  \\_______/|_______/ |__/|__/  |__/ \\______/\r\n" +
-                        "                                   /$$  | $$                                                        \r\n" +
-                        "                                  |  $$$$$$/                                                        \r\n" +
-                        "                                   \\______/                                                         ");
-        
-        
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            // Fallback (just print blank lines)
+            for (int i = 0; i < 50; i++) System.out.println();
+        }
+        System.out.println("██████  ██ ████████ ██████  ██    ██ ████████ ███████      ██████  █████  ███████ ██ ███    ██  ██████ \r\n" + 
+                        "██   ██ ██    ██    ██   ██  ██  ██     ██    ██          ██      ██   ██ ██      ██ ████   ██ ██    ██ \r\n" + 
+                        "██████  ██    ██    ██████    ████      ██    █████       ██      ███████ ███████ ██ ██ ██  ██ ██    ██ \r\n" + 
+                        "██   ██ ██    ██    ██   ██    ██       ██    ██          ██      ██   ██      ██ ██ ██  ██ ██ ██    ██ \r\n" + 
+                        "██████  ██    ██    ██████     ██       ██    ███████      ██████ ██   ██ ███████ ██ ██   ████  ██████  ");
         do {
             System.out.println("==============================================================================================");
             System.out.printf("\t\t\t\t\tBalance: PHP%.2f \n" ,balance);
@@ -37,8 +40,8 @@ public class BitByteCasino {
             System.out.println("\t\t\t\t\t2.  Blackjack");
             System.out.println("\t\t\t\t\t3.  Slot Machine");
             System.out.println("\t\t\t\t\t4.  Color Game");
-             System.out.println("\t\t\t\t\t5.  Crazy Time");
-             System.out.println("\t\t\t\t\t6.  Roulette");
+            System.out.println("\t\t\t\t\t5.  Crazy Time");
+            System.out.println("\t\t\t\t\t6.  Roulette");
             System.out.println("\t\t\t\t\t7.  Exit");
             System.out.println("----------------------------------------------------------------------------------------------");
             System.out.print("\t\t\tEnter your choice: ");
